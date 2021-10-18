0841579f2fce7eae37b79b6549c01692136b68f3
Jamey <75303006+Jaim010@users.noreply.github.com>
2021-10-18 13:47:11 +0200

    Added component: Text
    Component::Text allows you to change the text on the web page dynamically.The initial value of the page get send from Parent -> Child(Text component). The changed text will be send back to the parent component after pressing the button confirm.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,21 +1,46 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import Text from "./components/Text.vue";
+
+const allText = ref([
+  {
+    id: 0,
+    content:
+      "Bij bloedonderzoek nemen we een of meer buisjes bloed af met een hol naaldje. Dit bloed wordt vervolgens in het laboratorium onderzocht door een analist.",
+  },
+  {
+    id: 1,
+    content:
+      "Bloedonderzoek of onderzoek van urine of ontlasting is nodig wanneer de huisarts of specialist meer te weten wil komen over uw gezondheid. De arts laat ook onderzoek doen om het verloop van ziekte en het effect van een behandeling te bepalen. ",
+  },
+]);
+
+function changeValue(text: string, index: number) {
+  allText.value[index].content = text;
+}
+
+function testState() {
+  allText.value.forEach(function (value) {
+    console.log(value);
+  });
+}
+</script>
+
 <template>
   <div>
     <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <br />
+    <button @click="testState">Test Parent Component State</button>
+    <Text
+      v-for="textObj in allText"
+      :key="textObj.id"
+      :compText="textObj.content"
+      :index="textObj.id"
+      @value-change="changeValue"
+    />
+    <!-- <Text :compText="allText[1]" :index="1" @value-change="changeValue" /> -->
   </div>
 </template>
-
-<script lang="ts">
-import { Options, Vue } from "vue-class-component";
-import HelloWorld from "./components/HelloWorld.vue";
-
-@Options({
-  components: {
-    HelloWorld,
-  },
-})
-export default class App extends Vue {}
-</script>
 
 <style>
 #app {
