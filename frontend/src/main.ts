80e1147196cc6fa9eeaa8230df0e679416a2ecb6
wouter <wouterschiedam98@gmail.com>
2021-10-30 01:28:29 +0200

    Almost done, only making the google authenticator return TRUE

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,9 +1,9 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from './router';
-import OtpInput from "@bachdgvn/vue-otp-input";
+
 import { Vue } from "vue-class-component";
 
 
-createApp(App).use(router).component("v-otp-input", OtpInput).mount("#app");
+createApp(App).use(router).mount("#app");
 
