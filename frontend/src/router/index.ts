d15aa735197e1d30bcc52ff37e41f48314551be3
Djoowel <74959812+Djoowel@users.noreply.github.com>
2021-11-16 12:00:12 +0100

    Final version

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,6 +1,9 @@
 import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router'
-import Home from '../views/Home.vue'
-import Bloedprikken from '../views/Bloedprikken.vue'
+import Home from '../components/Home.vue'
+import Bloedprikken from '../components/Bloedprikken.vue'
+import Login from '../components/Login.vue'
+import twoFASetup from '../components/2FAsetup.vue'
+import twoFAverify from '../components/Verify2FA.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -15,7 +18,26 @@ const routes: Array<RouteRecordRaw> = [
     path: '/bloedprikken',
     name: 'Bloedprikken',
     component: Bloedprikken
-  }
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    props: true,
+    component: Login
+
+},
+{
+    path: '/twoFASetup',
+    name: 'twoFA',
+    props: true,
+    component: twoFASetup
+},
+{
+    path: '/Verify2FA',
+    name: 'verify2FA',
+    props: true,
+    component: twoFAverify
+}
 ]
 
 const router = createRouter({
