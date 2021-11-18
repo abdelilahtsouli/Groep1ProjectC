34b8a5ce4defb4e0a35842003cc7bbdddffaa66f
Jamey <75303006+Jaim010@users.noreply.github.com>
2021-11-18 21:17:06 +0100

    Started bloedprikken page

--- frontend/src/components/Bloedprikken.vue
+++ frontend/src/components/Bloedprikken.vue
@@ -1,43 +1,86 @@
 <template>
-  <div class="bloedprikken">
-    <div v-for="page in pages" :key="page.Id">
-      <div v-if="page.Id == 2">
-        <div v-for="media in page.Media" :key="media.Id">
-          <div v-if="media.Type == 'image'">
-            <img :class="['img-' + media.Location]" :src="media.Path" />
-          </div>
-          <div v-if="media.Type == 'video'">
-            <span
-              :class="['vid-' + media.Location]"
-              v-html="media.Video_HTML"
-            ></span>
-          </div>
-          <div v-else>
-            <p :class="['txt-' + media.Location]">{{ media.Content }}</p>
-          </div>
-        </div>
-      </div>
+  <div class="white-space-top"></div>
+  <div class="container">
+    <div id="demo">
+      <button @click="show = !show">Toggle</button>
+
+      <transition name="fade">
+        <p v-show="show">hello</p>
+      </transition>
+    </div>
+    <div class="text-box">
+      <h3>Placeholder</h3>
+      <p>
+        Op een klein deel werken we voor bloedafname volgens geplande afspraken.
+        Op deze pagina kunt u direct online een afspraak inplannen voor
+        bloedafname op de locaties waar een afspraak nodig is. In dit overzicht
+        ziet u in de kolom 'Bijzonderheden' welke locaties dit zijn. Wilt u een
+        afspraak maken voor bloedafname voor NIPT? Bekijk dan eerst hier op
+        welke locaties dat kan.
+      </p>
+      <h3>Placeholder</h3>
+      <p>
+        Op een klein deel werken we voor bloedafname volgens geplande afspraken.
+        Op deze pagina kunt u direct online een afspraak inplannen voor
+        bloedafname op de locaties waar een afspraak nodig is. In dit overzicht
+        ziet u in de kolom 'Bijzonderheden' welke locaties dit zijn. Wilt u een
+        afspraak maken voor bloedafname voor NIPT? Bekijk dan eerst hier op
+        welke locaties dat kan.
+      </p>
+      <h3>Placeholder</h3>
+      <p>
+        Op een klein deel werken we voor bloedafname volgens geplande afspraken.
+        Op deze pagina kunt u direct online een afspraak inplannen voor
+        bloedafname op de locaties waar een afspraak nodig is. In dit overzicht
+        ziet u in de kolom 'Bijzonderheden' welke locaties dit zijn. Wilt u een
+        afspraak maken voor bloedafname voor NIPT? Bekijk dan eerst hier op
+        welke locaties dat kan.
+      </p>
+      <h3>Placeholder</h3>
+      <p>
+        Op een klein deel werken we voor bloedafname volgens geplande afspraken.
+        Op deze pagina kunt u direct online een afspraak inplannen voor
+        bloedafname op de locaties waar een afspraak nodig is. In dit overzicht
+        ziet u in de kolom 'Bijzonderheden' welke locaties dit zijn. Wilt u een
+        afspraak maken voor bloedafname voor NIPT? Bekijk dan eerst hier op
+        welke locaties dat kan.
+      </p>
+      <h3>Placeholder</h3>
+      <p>
+        Let op: heeft u een afspraak gemaakt, maar kunt u niet naar de afspraak
+        komen? Annuleer dan de afspraak via de link in de bevestigingsmail of
+        bel ons klantcontactcentrum. Doet u dit niet, dan kunnen er kosten in
+        rekening worden gebracht
+      </p>
     </div>
   </div>
+  <div class="white-space-bottom"></div>
 </template>
 
-<script lang="ts" setup>
-import { defineEmits, onMounted, ref } from "vue";
-import axios from "axios";
+<script lang="ts">
+import { defineComponent, onMounted, ref } from "vue";
 
-var pages = ref([]);
+export default defineComponent({
+  components: {},
+  setup(props, { emit }) {
+    onMounted(() => {
+      emit("switchPage", "bloedprikken");
+    });
 
-function fetchContent() {
-  const baseURI = "http://localhost:3000/pages";
-  axios.get(baseURI).then((result) => {
-    pages.value = result.data;
-  });
-}
-
-const emit = defineEmits(["switchPage"]);
-
-onMounted(() => {
-  fetchContent();
-  emit("switchPage", "bloedprikken");
+    const show = ref(true);
+    return { show };
+  },
 });
 </script>
+
+<style scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
