2295b5d292cb0915d6be2c875705ad1f1ea6b77c
wouterschiedam <79146984+wouterschiedam@users.noreply.github.com>
2021-12-01 17:30:23 +0100

    beloofd

--- frontend/src/components/Location.vue
+++ frontend/src/components/Location.vue
@@ -1,13 +1,15 @@
 <template>
   <div class="content-location">
-    <h1>Locaties bloedafname</h1>
-    <h4>Coronamaatregelen op onze locaties</h4>
-    <p>Star-shl blijft de anderhalve meter afstand handhaven. Op onze locaties komen mensen met een kwetsbare gezondheid. Het afstand houden beschermt deze patiënten tegen risico op overdracht van Covid-19. Daarnaast willen we voorkomen dat medewerkers besmet raken en niet kunnen werken.</p>
-    <br>
-    <h4>Mondkapjes verplicht</h4>
-    <p>Mondkapjes zijn sinds 6 november 2021 verplicht in publieke ruimtes. We vragen patiënten daarom dringend een zelf meegebracht mondkapje te dragen op de locaties van Star-shl. Star-shl volgt hierin het beleid van de Rijksoverheid. Onze medewerkers maken gebruik van een mondneusmasker en handschoenen.</p>
-    <br>
-    <p>Op deze pagina vindt u alle locaties voor bloedafname.</p>
+    <div class="text-content">
+      <h1>Locaties bloedafname</h1>
+      <h4>Coronamaatregelen op onze locaties</h4>
+      <p>Star-shl blijft de anderhalve meter afstand handhaven. Op onze locaties komen mensen met een kwetsbare gezondheid. Het afstand houden beschermt deze patiënten tegen risico op overdracht van Covid-19. Daarnaast willen we voorkomen dat medewerkers besmet raken en niet kunnen werken.</p>
+      <br>
+      <h4>Mondkapjes verplicht</h4>
+      <p>Mondkapjes zijn sinds 6 november 2021 verplicht in publieke ruimtes. We vragen patiënten daarom dringend een zelf meegebracht mondkapje te dragen op de locaties van Star-shl. Star-shl volgt hierin het beleid van de Rijksoverheid. Onze medewerkers maken gebruik van een mondneusmasker en handschoenen.</p>
+      <br>
+      <p>Op deze pagina vindt u alle locaties voor bloedafname.</p>
+    </div>
   </div>
   <div class="map-responsive">
     <iframe src="https://www.google.com/maps/d/embed?mid=1b_K2S4IW2EyIJf_2WR1jcqo5THYs8fGe&hl=nl&z=8" width="600" height="400" style="border:0" allowfullscreen></iframe>
@@ -17,10 +19,13 @@
 <style>
 
 .content-location{
-  margin: 0;
   margin-top: 100px;
   text-align: center;
   width: 100%;
+}
+.text-content p{
+  margin-left: 10px;
+  margin-right: 10px;
 }
 .map-responsive{
   margin: 15px 15px 80px 15px;
