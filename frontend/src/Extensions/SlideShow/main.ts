5519299efa24d2a6de10ef5faf39842fd2f9bc80
Jamey <jameyschaap06@gmail.com>
2021-12-16 19:41:04 +0100

    SlideShow\main.ts

--- frontend/src/Extensions/SlideShow/main.ts
+++ frontend/src/Extensions/SlideShow/main.ts
@@ -6,8 +6,8 @@ let slideIndex = 1;
 function setAll(): void {
   const prevButtons = document.getElementsByClassName("prev");
   const nextButtons = document.getElementsByClassName("next");
-  const dots = document.getElementsByClassName("dot");
-  const numbertexts = document.getElementsByClassName("numbertext");
+  setDotEvents();
+  setNumberText();
 
   Array.from(prevButtons).forEach(
     (button) =>
@@ -22,22 +22,6 @@ function setAll(): void {
         plusSlides(1);
       })
   );
-
-  const imgAmount = document.getElementsByClassName("mySlides").length;
-  let counter = 1;
-
-  Array.from(dots).forEach((dot, counter) => {
-    (<HTMLElement>dot).onclick = () => {
-      currentSlide(counter);
-    };
-    counter++;
-  });
-
-  counter = 1;
-  Array.from(numbertexts).forEach((text) => {
-    (<HTMLDivElement>text).innerText = `${counter} / ${imgAmount}`;
-    counter++;
-  });
 }
 
 function showSlides(index: number): void {
@@ -68,10 +52,81 @@ function currentSlide(index: number): void {
   showSlides((slideIndex = index));
 }
 
-function addSlide(): void {
-  //
+function setNumberText() {
+  const numbertexts = document.getElementsByClassName("numbertext");
+  const imgAmount = document.getElementsByClassName("mySlides").length;
+
+  let counter = 1;
+  Array.from(numbertexts).forEach((text) => {
+    (<HTMLDivElement>text).innerText = `${counter} / ${imgAmount}`;
+    counter++;
+  });
 }
 
-function removeSlide(): void {
-  //
+function setDotEvents() {
+  const dots = document.getElementsByClassName("dot");
+  Array.from(dots).forEach((dot, counter) => {
+    (<HTMLElement>dot).onclick = () => {
+      currentSlide(counter);
+    };
+    counter++;
+  });
+}
+
+function addDot(containerIndex: number) {
+  const dots = document.getElementsByName("dots")[containerIndex];
+  const newDot = document.createElement("div");
+  newDot.classList.add("dot");
+  dots.appendChild(newDot);
+}
+
+function removeDot(containerIndex: number) {
+  const dots = document.getElementsByName("dots")[containerIndex];
+  dots.removeChild(dots.getElementsByClassName("dot")[0]);
+}
+
+function addSlide(path: string, containerIndex: number): void {
+  const slideText = document.createElement("div");
+  slideText.classList.add("numbertext");
+
+  const image = document.createElement("img");
+  image.src = path;
+
+  const newSlide = document.createElement("div");
+  newSlide.classList.add("mySlides");
+  newSlide.classList.add("fade");
+  newSlide.appendChild(slideText);
+  newSlide.appendChild(image);
+
+  const slideshow = document.getElementsByClassName("slideshow-container")[
+    containerIndex
+  ];
+  const referenceNode =
+    slideshow.getElementsByClassName("mySlides")[slideIndex - 1];
+  slideshow.insertBefore(newSlide, referenceNode);
+
+  addDot(containerIndex);
+  setDotEvents();
+  setNumberText();
+  showSlides(slideIndex);
+}
+
+function removeSlide(containerIndex: number): void {
+  const slides = document.getElementsByClassName("mySlides");
+  if (slides.length > 1) {
+    const slideshow = document.getElementsByClassName("slideshow-container")[
+      containerIndex
+    ];
+    console.log(slideshow);
+    const slideToRemove =
+      slideshow.getElementsByClassName("mySlides")[slideIndex - 1];
+    console.log(slideToRemove);
+
+    slideshow.removeChild(slideToRemove);
+
+    removeDot(containerIndex);
+    setDotEvents();
+    setNumberText();
+    showSlides(slideIndex + 1);
+  }
 }
