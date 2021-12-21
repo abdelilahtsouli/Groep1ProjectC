15720e9d6434697baec7202544f6ddc085b24fd1
Wouter <Wouterschiedam98@gmail.com>
2021-12-21 11:20:04 +0100

    DIKKE VETTE MERGE

--- frontend/src/pages/Urineonderzoek.vue
+++ frontend/src/pages/Urineonderzoek.vue
@@ -4,47 +4,18 @@
         <img src="../assets/images/fotolaborant.jpg" class="img-banner">
         <h1 class="header-text">Urineonderzoek</h1>
     </div>
-    <div class="uneditableContent">
-        <h3>Doel onderzoek</h3>
-            <p>
-                Om meer te weten te komen over uw gezondheid, 
-                heeft uw huisarts een laboratoriumonderzoek aangevraagd waarbij de test(en) plaats vinden uit een urinemonster.
-            </p>
-        <h3>Voorbereiding</h3>
-            <p>
-                Een urinepotje en de urinebuisjes die u nodig heeft, ontvangt u van de huisarts of van een medewerker op 
-                een locatie van Star-shl. In het urinepotje zit een geïntegreerd afnamesysteem om de urine in de buisjes over te brengen.
-                Wij wijzen u erop dat dit een naald is.
-            </p>
-        <h3>Verloop onderzoek</h3>
-            <p>
-                Vang een beetje urine op in het speciale urinepotje. Voor het opvangen van de urine moet u bij voorkeur de hele nacht
-                of ten minste gedurende drie uren niet geplast hebben. Dat kan het beste meteen na het opstaan.<br><br>
-                De urine is dan het meest geconcentreerd en afwijkingen worden daarin sneller opgespoord. Wast u zich wel eerst, 
-                zodat er geen bacteriën of huidcellen in terechtkomen.<br><br>
-                Het beste monster wordt verkregen door eerst een beetje uit te plassen in de wc en daarna wat urine op te 
-                vangen in het potje.
-                Vervolgens dient u volgens onderstaande instructie de daarvoor bestemde urinebuisje(s) te vullen.<br><br>
-
-                U dient op het urinepotje, met geïntegreerd afnamesysteem, en op de gevulde buizen de volgende gegevens vermelden:
-                naam + voorletters, geboortedatum, geslacht, plasdatum en plastijd
-                Het materiaal moet zo snel mogelijk afgegeven worden op één van de Star-shl locaties.
-            </p>
-        <!-- HIER MOET DE SLIDE SHOW KOMEN -->
-        <h3>Doorlooptijden en uitslag</h3>
-            <p>
-                Dit onderzoek gebeurt op verzoek van de huisarts. De resultaten worden aan de huisarts gezonden. 
-                Hij/zij zal de uitslag met u bespreken. Voor de meeste onderzoeken duurt het enkele dagen tot 
-                een week voordat de uitslagen bij uw huisarts zijn.
-                <br><br>
-                <b>Wachttijd:</b> Niet van toepassing
-                <br>
-                <b>Leeftijdsgrens:</b> Niet van toepassing
-            </p>
-    </div>
-  <!-- <page-content :id="2"></page-content> -->
+  <page-content :id="6"></page-content>
   <div class="white-space-bottom"></div>
 </template>
+<script lang="ts">
+import PageContent from "../components/PageContent.vue";
+import { defineComponent } from "vue";
+export default defineComponent({
+  components: {
+    PageContent,
+  },
+});
+</script>
 
 <style>
 .img-field{
