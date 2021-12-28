5bf46af50f2d0bd5445000ce4430033ff836a5be
Jamey <jameyschaap06@gmail.com>
2021-12-28 22:34:38 +0100

    Changed discord bot name

--- backup-app/src/postRequest.js
+++ backup-app/src/postRequest.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 const axiosPost = (content) =>
   axios
   .post("https://discord.com/api/webhooks/925486984535879741/qNWCq9ewH0xDc_VpFyOZvYU6oV9BQLCDPReKAfiSDcYb96GFVTxx70H6iLpTlHOChFIa", {
-    username: "Spidey Bot",
+    username: "Bounce by the Ounce Bot",
     avatar_url: "https://i.ytimg.com/vi/PzP1XC51kro/maxresdefault.jpg",
     content: content
   }, {
